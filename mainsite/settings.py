4cc1c4b554290df138c7402d6a2530bd21ec3b22
rathorabhishek <54003495+rathorabhishek@users.noreply.github.com>
2024-12-31 15:35:01 +0530

    Update settings.py

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'travelapp',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': 'your-rds-endpoint',
+        'HOST': 'localhost',
         'PORT': '5432',
 
     }
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
